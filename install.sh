82d635afa1b192b73292b4ff131db5ea9dc77060
sudurraaa <sudurrahah@protonmail.com>
2022-04-11 23:49:11 +0400

    removedd install.sh(useless)

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p ~/.config/{bspwm,sxhkd,nvim,qtile,polybar}
-mv ~/.bashrc ~/.bashrc.bac
-mv bash/.bashrc ~/
-mv bspwm-sxhkd/bspwmrc ~/.config/bspwm/
-mv bspwm-sxhkd/sxhkdrc ~/.config/sxhdk/
-mv nvim/* ~/.config/nvim/
-mv polybar/config.ini ~/.config/polybar/
-mv qtile/config.py ~/.config/qtile/
